139c07141940b64405b591d1dbd05f4a740ddd8c
Patricia González García <patriciagonzalezinfo@gmail.com>
2025-11-06 17:33:16 +0100

    Cambiar musica entre escenas, menu y final, añadir luces 2d

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
@@ -10,7 +12,13 @@ public class MusicManager : MonoBehaviour
 
     #region Fields
     public static MusicManager Instance { get; private set; }
+
     [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _backgroundMusicClip;
+    [SerializeField] private AudioClip _menuMusicClip;
+    [SerializeField] private AudioClip _finalMusicClip;
+
+    private string _currentSceneName;
     #endregion
 
     private void Awake()
@@ -27,7 +35,38 @@ public class MusicManager : MonoBehaviour
 
             bool isMusicOff = PlayerPrefs.GetInt("MusicOff", 0) == 1;
             _audioSource.mute = isMusicOff;
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        _currentSceneName = scene.name;
+
+        if (_currentSceneName == "Menu")
+        {
+            PlayMusic(_menuMusicClip, 1f, true);
+        }
+        else if (_currentSceneName == "Final")
+        {
+            PlayMusic(_finalMusicClip, 0.6f, false);
+        }
+        else
+        {
+            PlayMusic(_backgroundMusicClip, 0.3f, true);
+        }
+    }
+
+    private void PlayMusic(AudioClip clip, float volume, bool loop)
+    {
+        if (clip == null)
+            return;
+
+        _audioSource.clip = clip;
+        _audioSource.volume = volume;
+        _audioSource.loop = loop;
+        _audioSource.Play();
     }
 
     public void ToggleMusic()
